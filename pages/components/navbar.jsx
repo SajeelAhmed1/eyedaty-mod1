ee2ebd482338b0b050d3b19308ce4f757fd389eb
SajeelAhmed1 <sajeelahmedsiddiqui@gmail.com>
2022-10-20 02:47:37 +0500

    second commit

--- pages/components/navbar.jsx
+++ pages/components/navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar() {
         <div className='hidden lg:block w-2/5'>
             <input class=" placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
         </div>
-        <div className='hidden lg:blockflex  w-2/4 justify-evenly'>
+        <div className='hidden lg:flex  w-2/4 justify-evenly'>
             <button className="inline-flex items-center   border-2 border-siteblue  px-7 focus:outline-none hover:bg-gray-200 rounded-lg text-base mt-4 md:mt-0">Button
                 
             </button>
